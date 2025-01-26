03b25b6039de88ff6d0ba515c98cfb0d0e549689
Fahmudul Hassan <fahmudul234@gmail.com>
2025-01-27 04:01:28 +0600

    added register component

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import CustomForm from "../../components/CustomForm/CustomForm";
+import CustomInput from "../../components/CustomForm/CustomInput";
+import { Button } from "antd";
 
 const Register = () => {
+  const onSubmit = (data) => {};
+  console.log(data);
   return (
-    <div>
-      this is register page
-    </div>
-  )
-}
+    <CustomForm onSubmit={onSubmit}>
+      <CustomInput name="email" label="Email" type="email" />
+      <CustomInput name="password" label="Password" type="password" />
+      <CustomInput
+        name="confirmPassword"
+        label="Confirm Password"
+        type="password"
+      />
+      <Button htmlType="submit">Register</Button>
+    </CustomForm>
+  );
+};
 
-export default Register
+export default Register;
